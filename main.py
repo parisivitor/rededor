1936964b6727d9f19ffc093a4a752f71e2cb5e2e
vitor_parisi <parisivitor95@gmail.com>
2024-01-05 10:19:41 -0300

    Fix: Docker imagem push to docker hub

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def read_root():
     return {"message": "Hello, World!"}
 
 
-if __name__ == '__main__':
-    import uvicorn
+# if __name__ == '__main__':
+#     import uvicorn
 
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, log_level='info', reload=True)
+#     uvicorn.run("main:app", host="0.0.0.0", port=8000, log_level='info', reload=True)
