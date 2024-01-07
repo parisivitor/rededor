940f0afa76957d311c069fb1d6b449ab34a04093
vitor_parisi <parisivitor95@gmail.com>
2024-01-07 10:25:07 -0300

    improve cd and test coverage

--- main.py
+++ main.py
@@ -5,7 +5,3 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"message": "Projetc Rede D`or with CI/CD, Terraform on aws! Everything works!!!!"}
-
-@app.get("/end")
-def read_root():
-    return {"message": "AGORA FOI CARALHOOOOOO!!!!!!!!!!!!!!"}
