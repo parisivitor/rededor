2808016e7cf200c5992b75b27e390d291bbd46bd
vitor_parisi <parisivitor95@gmail.com>
2024-01-07 21:58:15 -0300

    feat: change message home

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "Project Rede D`or with CI/CD, Terraform on aws!"}
+    return {"message": "Project Rede D`or with CI/CD, Terraform on aws! Showing in video"}
--- src/user/tests/test_routes.py
+++ src/user/tests/test_routes.py
@@ -3,4 +3,4 @@ import requests
 def test_status_code_home():
     response = requests.get('http://127.0.0.1:8000')
     assert response.status_code == 200
-    assert response.json()['message'] == "Project Rede D`or with CI/CD, Terraform on aws!"
+    assert response.json()['message'] == "Project Rede D`or with CI/CD, Terraform on aws! Showing in video"
