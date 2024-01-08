c83c9e10b60718ab4188eea380b4631b17dd0d54
vitor_parisi <parisivitor95@gmail.com>
2024-01-07 21:59:43 -0300

    feat: change message home

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "Project Rede D`or with CI/CD, Terraform on aws! Showing in video"}
+    return {"message": "Project Rede D`or with CI/CD, Terraform on aws! It`s over! Let`s bora!!!"}
